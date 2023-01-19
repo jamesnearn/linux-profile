171f7a63477485df9b612e43336f6060cf909ce6
James <james@office01.home>
2023-01-19 15:01:57 -0500

    remove old scripts

--- scripts/cinnamon-disable-screensaver-and-lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gsettings set org.cinnamon.desktop.session idle-delay 1800
-gsettings set org.cinnamon.desktop.screensaver lock-enabled false
-
-
--- scripts/cinnamon-enable-screensaver-and-lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gsettings set org.cinnamon.desktop.session idle-delay 300
-gsettings set org.cinnamon.desktop.screensaver lock-enabled true
-
-
